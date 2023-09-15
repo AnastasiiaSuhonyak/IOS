bb2fa98cfc3e568427758e08e9c6d1475f97fa6c
Anastasiia <106271125+AnastasiiaSuhonyak@users.noreply.github.com>
2023-09-15 21:24:35 +0300

    Add files via upload

---
+++ Lab2/Lab2/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Lab2
+//
+//  Created by Anastasiia Sukhoniak on 15.09.2023.
+//
+
+import SwiftUI
+
+struct Student: Identifiable {
+    var id = UUID()
+    var name: String
+    var imageName: String
+}
+
+struct ContentView: View {
+    let students = [
+        Student(name: "Котик", imageName: "cat_photo"),
+        Student(name: "Песик", imageName: "dog_photo"),
+        Student(name: "Качечка", imageName: "duck_photo")
+    ]
+
+    let staticBiographyText = "Це група кн-31, яка відправилась на відпочинок на Мальдівах"
+
+    var body: some View {
+        VStack {
+            List(students) { student in
+                HStack {
+                    Image(student.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+
+                    Text(student.name)
+                }
+            }
+            .listStyle(PlainListStyle())  // Optional: To remove default list styling
+
+            Image("maldives")  // Add the image for Maldives
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 300)
+                .clipShape(Circle())
+                .padding()
+
+            Text(staticBiographyText)
+                .padding()
+        }
+        .padding()
+        .navigationBarTitle("Список студентів", displayMode: .inline)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab2/Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Anastasiia Sukhoniak on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
